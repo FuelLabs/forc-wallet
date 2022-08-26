6fa8815bb8314bc9eaaf48a0c3f7e97c4f94b474
kayagokalp <kaya.gokalp@fuel.sh>
2022-08-27 01:19:04 +0300

    Add plain address to the list

--- src/list.rs
+++ src/list.rs
@@ -23,12 +23,13 @@ pub(crate) fn print_wallet_list(path: Option<String>) -> Result<(), Error> {
         None => home::home_dir().unwrap().join(DEFAULT_WALLETS_VAULT_PATH),
     };
     let wallets = get_wallets_list(&wallet_path)?;
-    println!("#   address\n");
+    println!("#   address                                                         plain address\n");
     for wallet in wallets {
         let (index, address) = wallet;
         println!(
-            "[{}] 0x{}",
+            "[{}] {} 0x{}",
             index,
+            address,
             Bech32Address::from_str(&address)?.hash()
         )
     }
