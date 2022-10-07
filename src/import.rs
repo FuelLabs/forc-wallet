6bc4b445eee96eb80a2d2ff3ad9ceb5598ab3e36
Kaya Gokalp <kaya.gokalp@fuel.sh>
2022-10-07 19:41:26 +0300

    split import cli and check mnemonic

--- src/import.rs
+++ src/import.rs
@@ -1,26 +1,43 @@
+use crate::utils::{
+    default_vault_path, request_new_password, save_phrase_to_disk, validate_vault_path,
+};
+use anyhow::{bail, Result};
+use fuels::signers::wallet::WalletUnlocked;
 use std::path::PathBuf;
 
-use crate::{
-    utils::{default_vault_path, request_new_password, save_phrase_to_disk, validate_vault_path},
-    Error,
-};
-use fuels::signers::wallet::WalletUnlocked;
+/// Check if given mnemonic is valid by trying to create a `WalletUnlocked` from it
+fn check_mnemonic(mnemonic: &str) -> Result<()> {
+    // Check users's phrase by trying to create a wallet from it
+    if WalletUnlocked::new_from_mnemonic_phrase(mnemonic, None).is_err() {
+        bail!("Cannot generate a wallet from provided mnemonics, please check your mnemonic phrase")
+    }
+    Ok(())
+}
 
-pub(crate) fn import_wallet_cli(path_opt: Option<String>) -> Result<(), Error> {
+pub(crate) fn import_wallet_cli(path_opt: Option<String>) -> Result<()> {
     let path = path_opt.map_or_else(default_vault_path, PathBuf::from);
     validate_vault_path(&path)?;
     let mnemonic = rpassword::prompt_password("Please enter your mnemonic phrase: ")?;
-    // Check users's phrase by trying to create a wallet from it
-    if WalletUnlocked::new_from_mnemonic_phrase(&mnemonic, None).is_err() {
-        return Err(Error::WalletError(
-            "Cannot generate a wallet from provided mnemonics, please \
-        check your mnemonic phrase"
-                .to_string(),
-        ));
-    }
+    check_mnemonic(&mnemonic)?;
     let password = request_new_password();
     // Encyrpt and store it
     save_phrase_to_disk(&path, &mnemonic, &password);
-
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils::test_utils::TEST_MNEMONIC;
+
+    #[test]
+    fn check_mnemonic_should_succeed() {
+        assert!(check_mnemonic(TEST_MNEMONIC).is_ok())
+    }
+
+    #[test]
+    fn check_mnemonic_should_fail() {
+        let invalid_mnemonic = "this is an invalid mnemonic";
+        assert!(check_mnemonic(invalid_mnemonic).is_err())
+    }
 }
