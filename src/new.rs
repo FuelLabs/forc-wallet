e7991bd8f6c7a27ce9d35a48a901095b99d90d77
kayagokalp <kaya.gokalp@fuel.sh>
2023-07-04 16:01:36 +0300

    bump fuels-* to 0.43

--- src/new.rs
+++ src/new.rs
@@ -1,12 +1,13 @@
 use crate::utils::{
     display_string_discreetly, request_new_password, write_wallet_from_mnemonic_and_password,
 };
+use fuels::prelude::*;
 use std::path::Path;
 
 pub fn new_wallet_cli(wallet_path: &Path) -> anyhow::Result<()> {
     let password = request_new_password();
     // Generate a random mnemonic phrase.
-    let mnemonic = fuels_signers::wallet::generate_mnemonic_phrase(&mut rand::thread_rng(), 24)?;
+    let mnemonic = generate_mnemonic_phrase(&mut rand::thread_rng(), 24)?;
     write_wallet_from_mnemonic_and_password(wallet_path, &mnemonic, &password)?;
     let mnemonic_string = format!("Wallet mnemonic phrase: {mnemonic}\n");
     display_string_discreetly(
